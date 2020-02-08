fafe904c6ae7573c8029f52a32b6d44622de995d
Hiram <hiramgoodridge@gmail.com>
2020-02-07 22:52:58 -0500

    more changes

--- script.js
+++ script.js
@@ -10,16 +10,6 @@ function writePassword() {
   passwordText.value = password;
 }
 
-//
-//add 'if statments' 
-
-//  pwChoice1 = alert("Would you like to include Scecial characters in your password?")
-
-//  pwChoice2 = alert("Would you like to include uppercase characters in your password?")
-
-// pwChoice3 = alert("Would you like to include numbers in your password ?")
-
-// pwChoice4 = alert("Would you like to include lowercase characters in your password?")
 
 var specialChar = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '-', '"', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~']
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -27,68 +17,98 @@ var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 
+function getPrompt() {
+  var charLength = prompt("Enter a character length between 8-128");
+  console.log(charLength);
 
-// pool.specialChar[x]
-// pool.lowerCase[x]
-// pool.upperCase[x]
-// pool.nums[x]
-var charLength = prompt("Enter a character length between 8-128");
+  if (isNaN(charLength) === true) {
+    alert("Numbers only!")
+    return;
+  };
 
-function getPrompt(){
-console.log(charLength);
+  if (charLength < 8) {
+    alert("password min is 8");
+    return;
+  };
 
-if(isNaN(charLength) === true){
-  alert("Numbers only!")
+  if (charLength > 128) {
+    alert("max length is 128");
+    return;
+  };
+
+  var hasSpecCar = confirm("would you like special characters?");
+  var hasNums = confirm("would you like numbers?");
+  var hasUp = confirm("would you like uppercase characters");
+  var hasLow = confirm("would you like lower case characters");
+
+  if (hasSpecCar === false &&
+    hasNums === false &&
+    hasUp === false &&
+    hasLow === false) {
+    alert("must pick one!");
+    return;
+  };
+
+  var passOptions = {
+    hasSpecCar: hasSpecCar,
+    charLength: charLength,
+    hasUp: hasUp,
+    hasLow: hasLow,
+    hasNums: hasNums
+  };
+
+  console.log(passOptions)
+  return passOptions;
 };
-if(charLength < 8){
-alert("password min is 8");
-return;
+
+function getRanNum(arr) {
+  var indexofArray = Math.floor(Math.random() * arr.length);
+  var randEl = arr[indexofArray];
+  return randEl;
 };
 
-if(charLength > 128){
-alert("max length is 128");
-return;
-};
 
-var hasSpecCar = confirm("would you like special characters?");
-var hasNums = confirm("would you like numbers?");
-var hasUp = confirm("would you like uppercase characters");
-var hasLow = confirm("would you like lower case characters");
+function generatePassword() {
+  var options = getPrompt();
+  var result = [];
+  var possibleChars = [];
 
-if(hasSpecCar === false && 
-  hasNums === false &&
-  hasUp === false &&
-  hasLow === false){
-  alert("must pick one!");
-};
+  console.log(options)
 
-var passOptions = {
-  hasSpecCar: hasSpecCar,
-  charLength:charLength,
-  hasUp:hasUp,
-  hasLow:hasLow,
-  hasNums:hasNums
-};
-return passOptions;
-};
-var pwd = " "
-function getUp() {
-  for (var i = 0; i < charLength; i++) {
-
-    var x = Math.floor(Math.random() * upperCase.length);
-    pwd += upperCase[x]
-    console
-
+  // Conditional statement that adds array of special characters into array of possible characters based on user input
+  if (options.hasSpecCar === true) {
+    possibleChars.push(specialChar)
   }
+
+
+  //  Conditional statement that adds array of numeric characters into array of possible characters based on user input
+  if (options.hasNums === true) {
+    possibleChars.push(nums)
+  }
+
+  // Conditional statement that adds array of lowercase characters into array of possible characters based on user input
+
+if (options.hasLow === true) {
+possibleChars.push(lowerCase)
+  }
+  // Conditional statement that adds array of uppercase characters into array of possible characters based on user input
+if(options.hasUp === true) {
+  possibleChars.push(upperCase)
 }
-getUp();
+  // log possibleChars, get familiar with it
+  console.log(possibleChars)
 
+  // make a for loop that runs as many times as the user entered as their password length
+  for(i = 0; i < options.charLength; i++){
+    var ranArr = possibleChars[Math.floor(Math.random() * possibleChars.length)]
+    var ranChar = ranArr[Math.floor(Math.random() *  ranArr.length)]    
+    //   pick a random character from possibleChars and push it into the result array
+    result.push(ranChar)
+  }
+  // console.log result
 
+  return result.join("");
 
+}
 
-
-
-
-
-// // Add event listener to generate button
-// // generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
